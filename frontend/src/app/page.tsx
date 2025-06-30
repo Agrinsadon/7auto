7a0e08a1f90ea09884c23439371c097166231661
agrinsadon <sadon.code@gmail.com>
2025-06-30 10:11:56 +0300

    added a new info bar component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,11 @@
+import React from 'react';
+import InfoBar from './component/infoBar';
 
 export default function Home() {
   return (
     <div>
       Hello, World!
+      <InfoBar />
     </div>
   );
 }
