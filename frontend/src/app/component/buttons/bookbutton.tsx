010f87bf1e79bbd6f7742cf6e52e28d93ceb4bf9
agrinsadon <sadon.code@gmail.com>
2025-07-03 14:02:04 +0300

    added props for book button component and updated styles

--- frontend/src/app/component/buttons/bookbutton.tsx
+++ frontend/src/app/component/buttons/bookbutton.tsx
@@ -1,24 +1,19 @@
 "use client";
-import { useState } from "react";
 import "./bookbutton.css";
-import BookingFrame from "../bookframe/bookingframe";
 
-const BookButton = () => {
-    const [showFrame, setShowFrame] = useState(false);
+interface BookButtonProps {
+  onClick: () => void;
+  onText ?: string;
+}
 
-    const handleClick = () => {
-        setShowFrame(true); // show the booking frame
-    };
 
-    return (
-        <div className="book-button-container">
-            <button className="book-button" onClick={handleClick}>
-                <span className="button-text">Varaa aika</span>
-            </button>
 
-            {showFrame && <BookingFrame />}
-        </div>
-    );
-};
-
-export default BookButton;
+export default function BookButton({ onClick, onText }: BookButtonProps) {
+  return (
+    <div className="book-button-container">
+      <button className="book-button" onClick={onClick}>
+        <span className="button-text">{onText}</span>
+      </button>
+    </div>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import BookButton from './component/buttons/bookbutton';
+"use client";
+import React, { useState } from "react";
+import BookButton from "./component/buttons/bookbutton";
+import BookingFrame from "./component/bookframe/bookingframe";
 
 export default function Home() {
-  return (
-    <div>
-      <BookButton />
-    </div>
-  );
+    const [showFrame, setShowFrame] = useState(false);
+
+    const handleClick = () => {
+        setShowFrame(true);
+    };
+
+    return (
+        <div>
+            <BookButton onClick={handleClick} onText="Varaa aika" />
+            {showFrame && <BookingFrame />}
+        </div>
+    );
 }
