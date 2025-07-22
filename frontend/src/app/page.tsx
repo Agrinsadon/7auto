13d6f5e0d9590eccbbae041791c9947589a891a7
agrinsadon <sadon.code@gmail.com>
2025-07-22 15:58:15 +0300

    added more pages for carfix and carwash services

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,12 +3,16 @@
 import LandingPage from "./pages/landingpage/landingpage";
 import ImageGrid from "./pages/imagepage/imagepage";
 import ContactPage from "./pages/contactpage/contactpage";
+import Carwash from "./pages/services/carwash/carwash";
+import Carfix from "./pages/services/carfix/carfix";
 
 export default function Home() {
   return (
     <div>
       <LandingPage />
       <ImageGrid />
+      <Carwash />
+      <Carfix />
       <ContactPage />
     </div>
   );
---
+++ frontend/src/app/pages/services/carwash/carwash.tsx
@@ -0,0 +1,20 @@
+import './carwash.css';
+
+const Carwash = () => {
+
+    return (
+        <div className="carwash">
+        <div className="carwash-header">
+            <h1>CarWash</h1>
+            <p>Wash your car with ease</p>
+        </div>
+        <div className="carfix-content">
+            <p>CarWash is your go-to solution for all car repair needs. Whether it's a minor dent or a major engine issue, our team of experts is here to help you get back on the road quickly and safely.</p>
+            <p>We offer a wide range of services including bodywork, engine repairs, electrical diagnostics, and more. Our state-of-the-art facility is equipped with the latest technology to ensure your car is repaired to the highest standards.</p>
+            <p>Contact us today to schedule an appointment or to learn more about our services. We look forward to serving you!</p>
+        </div>
+        </div>
+)
+}
+
+export default Carwash;
