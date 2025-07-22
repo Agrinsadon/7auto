17f9e4d1d88fd8b152ed7d8160a560d0dd561d58
agrinsadon <sadon.code@gmail.com>
2025-07-22 16:51:25 +0300

    added new text for landing page and receipt page

--- frontend/src/app/pages/landingpage/landingpage.tsx
+++ frontend/src/app/pages/landingpage/landingpage.tsx
@@ -53,7 +53,8 @@ const LandingPage = () => {
       )}
 
     </div>
-    <p className='under-text'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    <p className='under-text'>Tarjoamme huollot ja pesut kätevästi saman katon alta. Olipa kyse öljynvaihdosta, jarrujen tarkistuksesta tai perusteellisesta sisä- ja ulkopesusta, hoidamme kaiken luotettavasti yhdestä osoitteesta. Käytämme laadukkaita tuotteita ja nykyaikaisia menetelmiä, jotta autosi on parhaassa mahdollisessa kunnossa sekä sisältä että ulkoa.</p>
+    <p className='under-text'>Varaa aika ja anna meidän huolehtia autostasi kokonaisvaltaisesti.</p>
     </>
   );
 };
