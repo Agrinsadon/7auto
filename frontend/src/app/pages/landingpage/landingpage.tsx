52180bbfc2d40954969e679895ef3ba0829de393
agrinsadon <sadon.code@gmail.com>
2025-07-23 11:27:11 +0300

    added a link to the text on landing page

--- frontend/src/app/pages/landingpage/landingpage.tsx
+++ frontend/src/app/pages/landingpage/landingpage.tsx
@@ -4,16 +4,22 @@ import './landingpage.css';
 import BookButton from '../../component/buttons/bookbutton'; 
 import BookingWash from '@/app/component/reservation-side/bookframes/bookingwash';
 import BookingFixStart from '@/app/component/reservation-side/bookframes/bookingfixstart';
+import BookingStart from '@/app/component/reservation-side/bookframes/bookingstart';
 
 const LandingPage = () => {
   const [isBookingWashOpen, setIsBookingWashOpen] = useState(false);
   const [isBookingFixOpen, setIsBookingFixOpen] = useState(false);
+  const [isBookingServicesOpen, setIsBookingServicesOpen] = useState(false);
 
   const handleCloseWash = () => setIsBookingWashOpen(false);
   const handleCloseFix = () => setIsBookingFixOpen(false);
 
+  const handleCloseServices = () => setIsBookingServicesOpen(false); // uusi sulkufunktio
+
   const handleOpenWash = () => setIsBookingWashOpen(true);
   const handleOpenFix = () => setIsBookingFixOpen(true);
+
+  const handleOpenServices = () => setIsBookingServicesOpen(true);
 
   return (
     <>
@@ -52,9 +58,24 @@ const LandingPage = () => {
         />
       )}
 
+      {isBookingServicesOpen && (
+          <BookingStart
+            onClose={handleCloseServices}
+          />
+        )}
+
     </div>
+    <div className="landing-text-frame">
     <p className='under-text'>Tarjoamme huollot ja pesut kätevästi saman katon alta. Olipa kyse öljynvaihdosta, jarrujen tarkistuksesta tai perusteellisesta sisä- ja ulkopesusta, hoidamme kaiken luotettavasti yhdestä osoitteesta. Käytämme laadukkaita tuotteita ja nykyaikaisia menetelmiä, jotta autosi on parhaassa mahdollisessa kunnossa sekä sisältä että ulkoa.</p>
-    <p>Katso vapaat ajat -</p>
+    <p 
+      className="booking-link" 
+      onClick={handleOpenServices}
+    >
+      Katso vapaat ajat&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;→
+    </p>
+
+
+    </div>
     </>
   );
 };
