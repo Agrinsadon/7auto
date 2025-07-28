23d90d046cf1ca165a0e63981c1700c65838021b
agrinsadon <sadon.code@gmail.com>
2025-07-28 15:13:50 +0300

    fixed footer styles

--- frontend/src/app/pages/footerpage/footerpage.tsx
+++ frontend/src/app/pages/footerpage/footerpage.tsx
@@ -12,24 +12,24 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-left">
+    <footer className="main-page-footer">
+      <div className="main-page-footer-left">
         {/* Logo */}
-        <div className="footer-logo">
-          <div className="footer-logo-wrapper">
+        <div className="main-page-footer-logo">
+          <div className="main-page-footer-logo-wrapper">
             <Image
               src="/logo-black.png"
               alt="7auto Logo"
               width={150}
               height={40}
-              className="footer-logo-img"
+              className="main-page-footer-logo-img"
               priority
             />
           </div>
         </div>
 
         {/* Social Media Icons */}
-        <div className="footer-socials">
+        <div className="main-page-footer-socials">
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
             <Facebook size={24} />
           </a>
@@ -39,21 +39,21 @@ const Footer = () => {
         </div>
 
         {/* Copyright */}
-        <p className="footer-copy">© 2025 Copyright 7auto Oy</p>
+        <p className="main-page-footer-copy">© 2025 Copyright 7auto Oy</p>
       </div>
 
-      <div className="footer-right">
-      <div className="footer-hours">
-          <h4 className="footer-title">Aukioloajat</h4>
+      <div className="main-page-footer-right">
+      <div className="main-page-footer-hours">
+          <h4 className="main-page-footer-title">Aukioloajat</h4>
           <p>Ma - Pe | 08:00 - 18:00</p>
           <p>La | 10:00 - 14:00</p>
           <p>Su | Suljettu</p>
         </div>
-        <div className="footer-contact">
-          <h4 className="footer-title">7auto</h4>
+        <div className="main-page-footer-contact">
+          <h4 className="main-page-footer-title">7auto</h4>
           <p>Y-tunnus: 1234567-8</p>
 
-          <div className="footer-info">
+          <div className="main-page-footer-info">
             <MapPin size={16} />
             <a
               href="https://www.google.com/maps/place/Helsinki,+Finland"
@@ -64,12 +64,12 @@ const Footer = () => {
             </a>
           </div>
 
-          <div className="footer-info">
+          <div className="main-page-footer-info">
             <Mail size={16} />
             <a href="mailto:info@7auto.fi">info@7auto.fi</a>
           </div>
 
-          <div className="footer-info">
+          <div className="main-page-footer-info">
             <Phone size={16} />
             <a href="tel:+358401234567">+358 40 123 4567</a>
           </div>
